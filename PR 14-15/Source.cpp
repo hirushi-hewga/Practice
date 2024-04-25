d31d6f6791765187d7eddc97d86a684367b90dc6
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-04-25 14:13:14 -0700

    PR 14-15

---
+++ PR 14-15/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	/*
+	int x = 2;
+	double H;
+	if (x > 1)
+		H = 2 * exp(3 * x);
+	else
+		H = abs(x + 1);
+	cout << "H = " << H << endl;
+
+
+	x = -2;
+	if (x > 1)
+		H = 2 * exp(3 * x);
+	else
+		H = abs(x + 1);
+	cout << "H = " << H << endl;
+	*/
+}
