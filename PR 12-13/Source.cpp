da41d03c7fa219eb442cadc16fe4dec73e24235e
Kuril_he35@student.itstep.org <Kuril_he35@student.itstep.org>
2024-04-25 13:51:53 -0700

    PR 12-13

--- PR 12-13/Source.cpp
+++ PR 12-13/Source.cpp
@@ -5,5 +5,18 @@ using namespace std;
 
 int main()
 {
+	/*
+	int V = 60, T = 5;
+	int S = V * T;
+	cout << "Way = " << S << "m." << endl;
+	*/
 
+
+	/*
+	double A, B, C;
+	cout << "A : "; cin >> A;
+	cout << "B : "; cin >> B;
+	C = (A + B) / 2;
+	cout << "C = " << C << endl;
+	*/
 }
